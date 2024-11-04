cc14ab2244d3106b102df79874ee1732dd57b030
SaravanaMuthuKumarS <saravanamuthukumar.sivasubramanian@ideas2it.com>
2024-11-04 18:26:53 +0530

    Feat: Created a initial structure for server using spring-boot

---
+++ server/src/main/java/com/i2i/smrb/SmrbApplication.java
@@ -0,0 +1,13 @@
+package com.i2i.smrb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmrbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmrbApplication.class, args);
+	}
+
+}
